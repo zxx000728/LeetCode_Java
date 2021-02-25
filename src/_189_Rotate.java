81ee9475173ade912f27f9a99beeec75fcdecab6
123 <nqxxzhuxiaoxuan@163.com>
2021-02-25 15:03:35 +0800

    189 rotate

---
+++ src/_189_Rotate.java
@@ -0,0 +1,17 @@
+public class _189_Rotate {
+    public void rotate(int[] nums, int k) {
+        int len = nums.length;
+        k = k % len;
+        reverse(nums, 0, len - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, len - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start++] = nums[end];
+            nums[end--] = temp;
+        }
+    }
+}
