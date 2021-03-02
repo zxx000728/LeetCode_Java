e9189f2e00ef4d581bb0963054cd2635ee65cbb2
123 <nqxxzhuxiaoxuan@163.com>
2021-03-02 15:23:13 +0800

    778 swimInWater

---
+++ src/_778_SwimInWater.java
@@ -0,0 +1,71 @@
+public class _778_SwimInWater {
+    private int N;
+    public static final int[][] DIRECTIONS = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    public int swimInWater(int[][] grid) {
+        this.N = grid.length;
+        int len = N * N;
+        int[] index = new int[len];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                index[grid[i][j]] = getIndex(i, j);
+            }
+        }
+        UnionFind unionFind = new UnionFind(len);
+        for (int i = 0; i < len; i++) {
+            int x = index[i] / N;
+            int y = index[i] % N;
+            for (int[] direction : DIRECTIONS) {
+                int newX = x + direction[0];
+                int newY = y + direction[1];
+                if (inArea(newX, newY) && grid[newX][newY] <= i) {
+                    unionFind.union(index[i], getIndex(newX, newY));
+                }
+                if (unionFind.isConnected(0, len - 1)) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    private int getIndex(int x, int y) {
+        return x * N + y;
+    }
+
+    private boolean inArea(int x, int y) {
+        return x >= 0 && x < N && y >= 0 && y < N;
+    }
+
+    private class UnionFind {
+        int[] parent;
+
+        public UnionFind(int n) {
+            this.parent = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+        }
+
+        public int find(int x) {
+            while (x != parent[x]) {
+                parent[x] = parent[parent[x]];
+                x = parent[x];
+            }
+            return x;
+        }
+
+        public boolean isConnected(int x, int y) {
+            return find(x) == find(y);
+        }
+
+        public void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX == rootY) {
+                return;
+            }
+            parent[rootX] = rootY;
+        }
+    }
+}
